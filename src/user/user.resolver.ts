e7d343735d653b63fdb2c7582bc50fa7e282a81e
Yesdoing <fkrmtleo12@gmail.com>
2021-03-07 16:29:57 +0900

    [feature] Create Account Mutation

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,8 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/createAccount.dto';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
@@ -9,5 +13,31 @@ export class UserResolver {
   @Query(() => Boolean)
   hi() {
     return true;
+  }
+
+  @Mutation(() => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const error = await this.userService.createAccount(createAccountInput);
+
+      if (error) {
+        return {
+          ok: false,
+          error,
+        };
+      }
+
+      return {
+        ok: true,
+        error: '',
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/createAccount.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -9,4 +10,22 @@ export class UserService {
     @InjectRepository(User)
     private readonly user: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
+    try {
+      const exist = await this.user.findOne({ email });
+
+      if (exist) {
+        return 'There is a user with that email already';
+      }
+
+      await this.user.save(this.user.create({ email, password, role }));
+    } catch (e) {
+      return "Couldn't create account";
+    }
+  }
 }
